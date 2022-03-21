54a1eef2b33093bdd529e1f75bed075e311eb7d1
lawal ridwan <lawalridwanullahi20@gmail.com>
2022-03-21 02:10:09 -0700

    initial commit

---
+++ three.js
@@ -0,0 +1,6 @@
+var numberX;
+console.log("i think the value of x is 5");
+console.log("the value of numberX is " +4);
+numberx=6;
+console.log("i think the value of numberX is 7");
+console.log("the value of numberX is " +9);
